40c1daf837eb54d845a03f5ba810122c5f97be46
Thang Le <amphale1996@gmail.com>
2016-01-30 19:18:45 -0500

    swap animation

--- cs-learning.js
+++ cs-learning.js
@@ -2,13 +2,17 @@
 var canvas = null;
 var ctx = null;
 var array;
+var mainY = 40;
+var SIZE = 20;
+var STEP = 5;
 
 // time variables
 var fps = 30;
 var then;
 var interval = 1000/fps;
 
-var start = false;
+// animationID
+var animationID;
 
 function bubbleSort(a) {
     var swapped;
@@ -28,20 +32,51 @@ function bubbleSort(a) {
 // Main
 $(document).ready(function() {
 	init();
+	swap(array, 0, 2);
 	draw();
 });
 
 // Functions
 function swap(a, i, j) {
-	var now = Date.now();
-	var timeDelta = now - then;
+	var destXLeft = a[i].x;
+	var destXRight = a[j].x;
+	var didMoveHorizontal = false;
 
-	if(timeDelta > interval) {
-		then = now - (timeDelta % interval);
-		draw();
-	}
-	
-	requestAnimationFrame(swap);
+	var swapAnimation = function() {
+		var now = Date.now();
+		var timeDelta = now - then;
+
+		if(timeDelta > interval) {
+			then = now - (timeDelta % interval);
+
+			// Move squares
+			if(a[i].y > mainY - SIZE && !didMoveHorizontal)
+				a[i].y -= STEP;
+			else if(a[i].x < destXRight)
+				a[i].x += STEP;
+			else if(a[i].y < mainY)
+				a[i].y += STEP;
+
+			if(a[j].y < mainY + SIZE && !didMoveHorizontal)
+				a[j].y += STEP;
+			else if(a[j].x > destXLeft) 
+				a[j].x -= STEP;
+			else if(a[j].y > mainY)
+				a[j].y -= STEP;
+			
+			if(a[i].x == destXRight && a[j].x == destXLeft) {
+				didMoveHorizontal = true;
+
+				if(a[i].y == mainY && a[j].y == mainY)
+					cancelAnimationFrame(animationID);
+			}
+			draw();
+		}
+
+		animationID = requestAnimationFrame(swapAnimation);
+	};	
+
+	requestAnimationFrame(swapAnimation);
 }
 
 function init() {
@@ -55,13 +90,17 @@ function init() {
 	then = Date.now();
 
 	// YOUR CODE HERE
-	array = [3, 2 ,5];
+	values = [3, 2, 5];
+	array = []
+	for(var i = 0; i < 3; i++) {
+		array.push(new Entry(i*SIZE, mainY, SIZE, SIZE, values[i]));
+	}
 }
 
 function draw() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	for(var i = 0; i < array.length; i++) {
-		ctx.strokeRect(i * 20, 20, 20, 20);
-		ctx.fillText(array[i], i*20 + 5, 20 + 20, 10);
+		ctx.strokeRect(array[i].x, array[i].y, array[i].width, array[i].height);
+		ctx.fillText(array[i].value, array[i].x + SIZE / 4, array[i].y + SIZE, array[i].width, array[i].height);
 	}
 }
